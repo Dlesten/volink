2fbd96c77f62a8b7e1db052629140118126774ee
Dlesten <deadllydoctor@gmail.com>
2024-12-13 23:08:05 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,28 @@ var swiper = new Swiper('.mySwiper', {
 		dynamicBullets: true
 	}
 });
+document.addEventListener('DOMContentLoaded', function () {
+	const burgerIcon = document.querySelector('.small-visually');
+	const menu = document.querySelector('.header__menu');
+	const menuLinks = document.querySelectorAll('.header__menu a');
+	const menuClose = document.querySelector('.menu__close');
+
+	burgerIcon.addEventListener('click', function () {
+		this.classList.toggle('active');
+		menu.classList.toggle('active');
+	});
+
+	menuLinks.forEach(link => {
+		link.addEventListener('click', function () {
+			burgerIcon.classList.remove('active');
+			menu.classList.remove('active');
+		});
+	});
+	burgerIcon.addEventListener('click', function () {
+		menu.classList.add('active');
+	});
+
+	menuClose.addEventListener('click', function () {
+		menu.classList.remove('active');
+	});
+});
